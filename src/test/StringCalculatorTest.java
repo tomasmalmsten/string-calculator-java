a605046a34b41f509cdafc347bcb5084c60fa342
Tomas Malmsten <tmalmsten@efolder.net>
2015-12-03 13:02:50 +0100

    ...

--- src/test/StringCalculatorTest.java
+++ src/test/StringCalculatorTest.java
@@ -1,9 +1,16 @@
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class StringCalculatorTest {
 
     @Test
     public void addingEmptyString_ReturnsZero() {
         Calculator calculator = new Calculator();
+        assertThat(calculator.add(""), is(0));
+    }
+
+    private class Calculator {
     }
 }
